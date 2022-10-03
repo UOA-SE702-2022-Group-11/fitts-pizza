d44e857bdbe37065db6ba0be79305a8445cad616
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-10-03 22:01:19 +1300

    feat ranking questions page

--- Script.js
+++ Script.js
@@ -5,5 +5,43 @@ function handleCompareContrastQuestionAnswers() {
   sessionStorage.setItem("compareContrast1", compareContrast1);
   sessionStorage.setItem("compareContrast2", compareContrast2);
   sessionStorage.setItem("compareContrast3", compareContrast3);
-  window.location.href = "../Test.html";
+  window.location.href = "ranking.html";
+}
+
+function handleRankingQuestions() {
+  let choices = [];
+  choices.push(document.getElementById("choice1").value);
+  choices.push(document.getElementById("choice2").value);
+  choices.push(document.getElementById("choice3").value);
+  choices.push(document.getElementById("choice4").value);
+  choices.push(document.getElementById("choice5").value);
+
+  // Checks no empty rankings
+  let checkEmpty;
+  let answeredCount = 0;
+  for (let i = 0; i < choices.length; i++) {
+    checkEmpty = document.getElementById(`choice${i + 1}`).validity.valid;
+    checkEmpty && (answeredCount = answeredCount + 1);
+  }
+  if (answeredCount == choices.length) {
+    document.getElementById("emptyRankingErrorMessage").style.display = "none";
+  } else {
+    document.getElementById("emptyRankingErrorMessage").style.display = "block";
+    return;
+  }
+
+  // Checks for duplicates
+  //reference https://stackoverflow.com/questions/7376598/in-javascript-how-do-i-check-if-an-array-has-duplicate-values
+  const checkDuplicates = new Set(choices).size !== choices.length;
+  if (checkDuplicates) {
+    document.getElementById("duplicateErrorMessage").style.display = "block";
+    return;
+  }
+
+  for (let i = 0; i < choices.length; i++) {
+    sessionStorage.setItem(
+      `rankingChoice${i + 1}`,
+      document.getElementById(`choice${i + 1}`).value
+    );
+  }
 }
