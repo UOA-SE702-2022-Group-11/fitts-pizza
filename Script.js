f07a8aa834f09268dfca84e66a8c38a9e121c921
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-10-08 15:32:29 +1300

    fix removed unneeded question for comparecontrast

--- Script.js
+++ Script.js
@@ -1,10 +1,8 @@
 function handleCompareContrastQuestionAnswers() {
   compareContrast1 = document.getElementById("compareContrast1").value;
   compareContrast2 = document.getElementById("compareContrast2").value;
-  compareContrast3 = document.getElementById("compareContrast3").value;
   sessionStorage.setItem("compareContrast1", compareContrast1);
   sessionStorage.setItem("compareContrast2", compareContrast2);
-  sessionStorage.setItem("compareContrast3", compareContrast3);
   window.location.href = "ranking.html";
 }
 
